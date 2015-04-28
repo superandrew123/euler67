d1498b96c2c36e23a7989da27e55afabda2f0969
Andrew Miksch <superandrew@Andrews-MacBook-Pro.local>
2015-04-28 13:30:23 -0400

    Currently not correct solution

--- big_triangle_path.rb
+++ big_triangle_path.rb
@@ -1,9 +1,26 @@
 class TriPathSolution
 
-  attr_accessor :triangle
+  attr_accessor :triangle, :result
 
   def initialize
     @triangle = TriReader.read
+    @result = 0
+    solve
+  end
+
+  def solve
+    position = 0
+    @triangle.each_with_index do |row, i|
+      if i == 0
+        @result = row[position]
+      elsif row[position] > row[position +1]
+        @result += row[position]
+      else
+        @result += row[position + 1]
+        position += 1
+      end
+    end
+    result
   end
 
 end
@@ -16,7 +33,7 @@ class TriReader
     tri_array = tri_string.split("\n")
     tri_array.each_with_index do |row, i|
       if i == 0
-        tri_array[i] = [row.to_i]
+        tri_array[i] = row.to_i
       end
       tri_array[i] = row.split(" ")
       tri_array[i].each_with_index do |num, j|
